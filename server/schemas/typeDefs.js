fd5ae185759b1b6c492c21bef3adb13d6fc25c15
Rosebud <rkle408@gmail.com>
2022-11-04 23:12:09 -0700

    started typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,36 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [bookSchema]!
+    }
 
+    type bookSchema {
+        _id: ID
+        authors: String
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+
+    }
+
+    type Mutation {
+        
+    }
 `;
 
 module.exports = typeDefs;
